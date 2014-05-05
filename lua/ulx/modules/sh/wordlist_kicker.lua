ef77c22a7e23ac1617e6e16c268971ed920d555a
Alex Thomassen <decicus@gmail.com>
2014-05-05 20:13:08 +0200

    Fixed error if wordlist-file doesn't exist. Minor rewording of variables. Removed function that runs on initialize. Now checks if the folder exists when the command is run.
    First test ended. Test was successful with no issues.

--- lua/ulx/modules/sh/wordlist_kicker.lua
+++ lua/ulx/modules/sh/wordlist_kicker.lua
@@ -3,48 +3,47 @@
 local dir = "data/wordlist_kicker/"
 local wordfile = "words.txt"
 local fread = ULib.fileRead( dir .. wordfile )
-local Wordlist = util.KeyValuesToTable( fread )
 
-function WKCheckFiles()
+if fread then
+
+	WKWordlist = util.KeyValuesToTable( fread )
 	
-	if not ULib.fileExists( dir ) then
-		
-		ULib.fileCreateDir( dir )
-		ULib.fileWrite( dir .. wordfile, "" )
-		
-	end
-	
+else -- KeyValuesToTable errors out if the file doesn't exist.
+
+	WKWordlist = {}
+
 end
-hook.Add( "Initialize", "WKCheckFiles", WKCheckFiles ) -- Makes sure that the files exist.
 
 function ulx.wkaddword( calling_ply, word, should_remove )
 
 	local word = string.lower( word ) -- All words should be saved lowercase.
 
+	if not ULib.fileExists( dir ) then ULib.fileCreateDir( dir ) end -- Create the folder (if non-existant) so the wordlist can save.
+	
 	if not should_remove then
 		
-		if table.HasValue( Wordlist, word ) then
+		if table.HasValue( WKWordlist, word ) then
 		
 			ULib.tsayError( calling_ply, "The word already exists in the wordlist." )
-		
+
 		else
 			
-			table.insert( Wordlist, word )
-			ULib.fileWrite( dir .. wordfile, util.TableToKeyValues( Wordlist ) )
+			table.insert( WKWordlist, word )
+			ULib.fileWrite( dir .. wordfile, util.TableToKeyValues( WKWordlist ) )
 			ulx.fancyLogAdmin( calling_ply, true, "#A added '#s' to the wordlist.", word )
 			
 		end
 		
 	else
 	
-		if not table.HasValue( Wordlist, word ) then
+		if not table.HasValue( WKWordlist, word ) then
 			
 			ULib.tsayError( calling_ply, "The word doesn't exist in the wordlist." )
 			
 		else
 		
-			table.RemoveByValue( Wordlist, word )
-			ULib.fileWrite( dir .. wordfile, util.TableToKeyValues( Wordlist ) )
+			table.RemoveByValue( WKWordlist, word )
+			ULib.fileWrite( dir .. wordfile, util.TableToKeyValues( WKWordlist ) )
 			ulx.fancyLogAdmin( calling_ply, true, "#A removed '#s' from the wordlist.", word )
 			
 		end
@@ -56,14 +55,14 @@ local addword = ulx.command( CATEGORY_NAME, "ulx addword", ulx.wkaddword, "!addw
 addword:addParam{ type=ULib.cmds.StringArg, hint="Word to blacklist" }
 addword:addParam{ type=ULib.cmds.BoolArg, invisible=true }
 addword:defaultAccess( ULib.ACCESS_SUPERADMIN )
-addword:help( "Adds a word to the Wordlist Kicker wordlist." )
+addword:help( "Adds a word to the WKWordlist Kicker WKWordlist." )
 addword:setOpposite( "ulx removeword", {_, _, true}, "!removeword", true )
 
 function WKCheckNameJoin( ply )
 	
 	local name = string.lower( ply:Nick() ) -- Lowercase makes it a lot easier with comparing.
 	
-	for k, word in pairs( Wordlist ) do
+	for k, word in pairs( WKWordlist ) do
 		
 		local word = string.lower( word ) -- This shouldn't be required unless someone has manually edited "words.txt".
 	
@@ -83,7 +82,7 @@ function WKCheckNameChange( ply, old, new )
 
 	local new = string.lower( new )
 	
-	for k, word in pairs( Wordlist ) do
+	for k, word in pairs( WKWordlist ) do
 	
 		local word = string.lower( word )
 		
